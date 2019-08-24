ad1c5f86106305060b8743ff1118cdb93f3e95ab
Joan Vitor <j.v.m.j.97@gmail.com>
2019-08-24 10:29:39 -0300

    Classe restricao horario criada

---
+++ AlgoritmoGenetico/Models/Dia.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Models
+{
+    public class Dia
+    {
+        public bool Horario1 { get; set; }
+        public bool Horario2 { get; set; }
+        public bool Horario3 { get; set; }
+    }
+}
---
+++ AlgoritmoGenetico/Models/RestricaoHorario.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Models
+{
+    [Table("tb_restricao_horario")]
+    public class RestricaoHorario
+    {
+        [Column("idrestricaohorario")]
+        public int Id { get; set; }
+        [NotMapped]
+        public Dia Segunda { get; set; }
+        [NotMapped]
+        public Dia Terca { get; set; }
+        [NotMapped]
+        public Dia Quarta { get; set; }
+        [NotMapped]
+        public Dia Quinta { get; set; }
+        [NotMapped]
+        public Dia Sexta { get; set; }
+        [Column("idano")]
+        public int IdAno { get; set; }
+        [ForeignKey("IdAno")]
+        public virtual Ano Ano { get; set; }
+
+        public RestricaoHorario()
+        {
+            Segunda = new Dia();
+            Terca = new Dia();
+            Quarta = new Dia();
+            Quinta = new Dia();
+            Sexta = new Dia();
+        }
+    }
+}
